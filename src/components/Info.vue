c13e3c00469e3ddf0df68a2b65013f6b82e3b45f
Jongho Jung <st168097@stud.uni-stuttgart.de>
2024-02-09 19:41:30 +0100

    add lotus and color code

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,21 +1,45 @@
 <template>
-        <v-container class="text-center">
-          <v-row justify="center">
-            <v-col cols="12" sm="8" md="6"  lg="4">
-    <v-card>
-        <v-card-title>Valorant Brimstone Lineups</v-card-title>
-        <v-card-subtitle>
-            Last Update : 2024-02-09
-        </v-card-subtitle>
-        <v-card-text>
-            1. Select Map<br>
-            2. Select Team<br>
-            3. Select Target<br>
-            * Click on the Image to Zoom<br>
-            <a href="https://github.com/Jung95/VAL_Brim">Github</a>
-        </v-card-text>
-    </v-card>
-    </v-col>
+  <v-container class="text-center">
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="6" lg="4">
+        <v-card>
+          <v-card-title>Valorant Brimstone Lineups</v-card-title>
+          <v-card-subtitle> Last Update : 2024-02-09 </v-card-subtitle>
+          <v-card-text>
+            1. Select Map<br />
+            2. Select Team<br />
+            3. Select Target<br />
+            * Click on the Image to Zoom<br />
+            * Color Code :
+            <div style="display: inline-flex; align-items: center">
+              <div
+                style="
+                  border: 2px solid red;
+                  height: 13px;
+                  width: 13px;
+                  border-radius: 50%;
+                  margin-right: 5px;
+                "
+              ></div>
+              Stand
+            </div>
+            /
+            <div style="display: inline-flex; align-items: center">
+              <div
+                style="
+                  border: 2px solid blue;
+                  height: 13px;
+                  width: 13px;
+                  border-radius: 50%;
+                  margin-right: 5px;
+                "
+              ></div>
+              Sit
+            </div>
+            <br /><a href="https://github.com/Jung95/VAL_Brim">Github</a>
+          </v-card-text>
+        </v-card>
+      </v-col>
     </v-row>
-    </v-container>
+  </v-container>
 </template>
